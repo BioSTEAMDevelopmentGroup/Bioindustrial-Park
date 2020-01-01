99ad2533a816de9e281343042a9a1c3d6bfa6bea
yoelcortes <yoelcortes@gmail.com>
2020-01-01 00:21:51 -0600

    update

---
+++ biorefineries/README.rst
@@ -0,0 +1,55 @@
+====================================================
+Biorefineries: BioSTEAM's Premier Biorefinery Models
+====================================================
+
+.. image:: http://img.shields.io/pypi/v/biorefineries.svg?style=flat
+   :target: https://pypi.python.org/pypi/biorefineries
+   :alt: Version_status
+.. image:: http://img.shields.io/badge/license-MIT-blue.svg?style=flat
+   :target: https://github.com/yoelcortes/Bioindustrial-Complex/biorefineries/blob/master/LICENSE.txt
+   :alt: license
+
+
+.. contents::
+
+What is Biorefineries?
+----------------------
+
+Biorefineries serves as a standard package for complete biorefinery models to foster accessibility and deeper communication within the BioSTEAM biorefinery simulation community.
+
+Installation
+------------
+
+Get the latest version of biorefineries from
+https://pypi.python.org/pypi/biorefineries/
+
+If you have an installation of Python with pip, simple install it with:
+
+    $ pip install biorefineries
+
+To get the git version, run:
+
+    $ git clone git://github.com/yoelcortes/Bioindustrial-Complex/biorefineries
+
+Documentation
+-------------
+
+Documentation for BioSTEAM's premier biorefinery models are available on the web:
+
+    http://biosteam.readthedocs.io/
+
+Bug reports
+-----------
+
+To report bugs, please use the biorefineries's Bug Tracker at:
+
+    git://github.com/yoelcortes/Bioindustrial-Complex/biorefineries
+
+
+License information
+-------------------
+
+See ``LICENSE.txt`` for information on the terms & conditions for usage
+of this software, and a DISCLAIMER OF ALL WARRANTIES.
+
+
---
+++ biorefineries/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 18 16:17:00 2017
+
+@author: Yoel Cortes-Pena
+"""
+from setuptools import setup
+#from Cython.Build import cythonize
+#import numpy
+
+setup(
+    name='biorefineries',
+    packages=['biorefineries'],
+    license='MIT',
+    version='0.1',
+    description="BioSTEAM's premier repository for biorefinery models",
+    long_description=open('README.rst').read(),
+    #ext_modules=cythonize('biosteam/equilibrium/unifac.pyx'),
+    #include_dirs=[numpy.get_include()],
+    author='Yoel Cortes-Pena',
+    install_requires=['biosteam'],
+    python_requires=">=3.6",
+    package_data=
+        {'biorefineries': ['biorefineries/*',
+                           'lipidcane/*', 
+                           'lipidcane/utils/*', 
+                           'lipidcane/species/*',
+                           'lipidcane/species/tripalmitin_liquid.xlsx', 
+                           'lipidcane/system/*', 
+                           'sugarcane/*',
+                           'cornstover/*', 
+                           'cornstover/_humbird2011.xlsx',
+                           'my_units_defs.txt', 
+                      ]},
+    platforms=['Windows', 'Mac', 'Linux'],
+    author_email='yoelcortes@gmail.com',
+    url='https://github.com/yoelcortes/Bioindustrial-Complex/biorefineries',
+    download_url='https://github.com/yoelcortes/Bioindustrial-Complex/biorefineries.git',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Environment :: Console',
+                 'License :: OSI Approved :: MIT',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Scientific/Engineering :: Chemistry',
+                 'Topic :: Scientific/Engineering :: Mathematics'],
+    keywords='chemical process simmulation bioprocess engineering mass CABBI biorefinery biofuel bioproducts lipid-cane corn stover',
+)
