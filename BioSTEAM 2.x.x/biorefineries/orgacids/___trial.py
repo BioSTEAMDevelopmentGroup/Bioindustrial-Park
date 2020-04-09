eb3b45c752d993d209545954e9ad1e3c22beacf2
Yalin Li <yalinli2@illinois.edu>
2020-04-09 15:54:03 -0500

    update

---
+++ BioSTEAM 2.x.x/biorefineries/orgacids/___trial.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 12:49:42 2020
+
+@author: yalinli_cabbi
+"""
+
+
+# %% Whole-system simulation
+
+'''
+biosteam v2.11.1
+thermosteam v0.10.2
+'''
+
+import biosteam as bst
+from orgacids.system import *
+
+find = bst.main_flowsheet
+broth_for_separation = find.broth_for_separation
+
+for i in range(5):
+    orgacids_sys.simulate()
+    # print(F401.outs[1].F_mass)
+    # Right now MPSP changes a bit each time the system simulates due to separation_sys
+    # starting F401, the outs change each simulation
+    MPSP = orgacids_tea.solve_price(lactic_acid, orgacids_sys_no_boiler_tea)
+    print(MPSP)
+
+#!!! M203.heat_utilities raises error
+#!!! save_report not saving diagrams
+# orgacids_sys.save_report('orgacids_sys.xlsx')
+
+
+
+
