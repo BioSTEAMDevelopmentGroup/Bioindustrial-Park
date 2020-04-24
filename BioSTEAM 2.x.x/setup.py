ffaf20f60ab64ab9d9a66bf5817944e9970495fd
yoelcortes <yoelcortes@gmail.com>
2020-04-24 11:33:33 -0500

    update

--- BioSTEAM 2.x.x/setup.py
+++ BioSTEAM 2.x.x/setup.py
@@ -10,7 +10,7 @@ setup(
     name='biorefineries',
     packages=['biorefineries'],
     license='MIT',
-    version='2.9.3',
+    version='2.9.4',
     description="Biorefinery models in BioSTEAM",
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
