2a236b783ac9d05384462d015ea979cf3809f343
Yoel <yoelcortes@gmail.com>
2020-09-11 23:38:32 -0500

    fix bug with fattyalcohols package

--- BioSTEAM 2.x.x/biorefineries/fattyalcohols/_system.py
+++ BioSTEAM 2.x.x/biorefineries/fattyalcohols/_system.py
@@ -7,23 +7,12 @@
 # for license details.
 """
 """
-import os
-import numpy as np
 import biosteam as bst
-import thermosteam as tmo
-from biosteam import units
-from biosteam.plots import plot_contour_2d, MetricBar, CABBI_green_colormap
-from matplotlib import pyplot as plt
-from warnings import filterwarnings
-import biorefineries.fattyalcohols as fa
-from biosteam import main_flowsheet as F
-from thermosteam.reaction import (Reaction as Rxn,
-                                  ParallelReaction as ParallelRxn,
-)
 
 __all__ = ('create_system',)
 
 def create_system(ID='fattyalcohol_sys'):
+    import biorefineries.fattyalcohols as fa
     chemicals = fa.create_chemicals()
     bst.settings.set_thermo(chemicals)
     fattyalcohol_production_sys = fa.create_fattyalcohol_production_sys()
