731e441baa9f13fdf61b6493618e351f40fe8bad
Yoel <yoelcortes@gmail.com>
2020-09-21 15:33:51 -0500

    add additional metrics to ABM interface

--- BioSTEAM 2.x.x/setup.py
+++ BioSTEAM 2.x.x/setup.py
@@ -11,7 +11,7 @@ setup(
     name='biorefineries',
     packages=['biorefineries'],
     license='MIT',
-    version='2.15.20',
+    version='2.15.21',
     description="Biorefinery models in BioSTEAM",
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
