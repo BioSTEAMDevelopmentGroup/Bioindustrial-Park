d1c6870af4e20501dea1e0a1e2d75ea2c85d4c62
Yoel <yoelcortes@gmail.com>
2020-12-25 16:16:41 -0600

    fix readme tests

--- BioSTEAM 2.x.x/biorefineries/tests/run_readmes.py
+++ BioSTEAM 2.x.x/biorefineries/tests/run_readmes.py
@@ -10,36 +10,47 @@
 
 import os
 from doctest import testfile
-from biorefineries import (
-    sugarcane as sc,
-    lipidcane as lc,
-    cornstover as cs,
-    LAOs as laos,
-    lactic,
-    ethanol_adipic,
-    )
+
 
 __all__ = (
-    'test_sugarcane_readme',
-    'test_lipidcane_readme',
-    'test_cornstover_readme',
-    'test_laos_readme',
-    'test_lactic_readme',
-    'test_ethanol_adipic_readme',
+    'run_sugarcane_readme',
+    'run_lipidcane_readme',
+    'run_cornstover_readme',
+    'run_laos_readme',
+    'run_lactic_readme',
+    'run_ethanol_adipic_readme',
 )
 
 get_readme = lambda module: os.path.join(os.path.dirname(module.__file__), 'README.rst')
-test_sugarcane_readme = lambda: testfile(get_readme(sc))
-test_lipidcane_readme = lambda: testfile(get_readme(lc))
-test_cornstover_readme = lambda: testfile(get_readme(cs))
-test_laos_readme = lambda: testfile(get_readme(laos))
-test_lactic_readme = lambda: testfile(get_readme(lactic))
-test_ethanol_adipic_readme = lambda: testfile(get_readme(ethanol_adipic))
+
+def run_sugarcane_readme():
+    from biorefineries import sugarcane as sc
+    testfile(get_readme(sc))
+    
+def run_lipidcane_readme():
+    from biorefineries import lipidcane as lc
+    testfile(get_readme(lc))
+
+def run_cornstover_readme():
+    from biorefineries import cornstover as cs
+    testfile(get_readme(cs))
+
+def run_laos_readme():
+    from biorefineries import LAOs as laos
+    runfile(get_readme(laos))
+    
+def run_lactic_readme():
+    from biorefineries import lactic
+    testfile(get_readme(lactic))
+    
+def run_ethanol_adipic_readme():
+    from biorefineries import ethanol_adipic
+    testfile(get_readme(ethanol_adipic))
 
 if __name__ == '__main__':
-    test_sugarcane_readme()
-    test_lipidcane_readme()
-    test_cornstover_readme()
-    test_laos_readme()
-    test_lactic_readme()
-    test_ethanol_adipic_readme()
+    run_sugarcane_readme()
+    run_lipidcane_readme()
+    run_cornstover_readme()
+    run_laos_readme()
+    run_lactic_readme()
+    run_ethanol_adipic_readme()
