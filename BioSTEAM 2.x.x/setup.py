07a354e0fecac010b7f68300cf8997b39ef554cf
Yoel <yoelcortes@gmail.com>
2021-05-01 22:40:40 -0500

    update biosteam requirement

--- BioSTEAM 2.x.x/setup.py
+++ BioSTEAM 2.x.x/setup.py
@@ -11,11 +11,11 @@ setup(
     name='biorefineries',
     packages=['biorefineries'],
     license='MIT',
-    version='2.21.3',
+    version='2.21.4',
     description="Biorefinery models in BioSTEAM",
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
-    install_requires=['biosteam>=2.28.3'],
+    install_requires=['biosteam>=2.28.4'],
     python_requires=">=3.6",
     package_data=
         {'biorefineries': ['biorefineries/*',
