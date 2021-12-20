6f7495b74e77ab926e66b7c73a211e7d1470b620
Yoel Cortes-Pena <yoelcortes@gmail.com>
2021-12-20 09:04:07 -0600

    update

--- BioSTEAM 2.x.x/biorefineries/ozonolysis/chemicals_info.py
+++ BioSTEAM 2.x.x/biorefineries/ozonolysis/chemicals_info.py
@@ -4,6 +4,7 @@
 import thermosteam as tmo
 import biosteam as bst
 from biosteam import Unit, Stream, settings, main_flowsheet
+
 #%% Chemicals - Definition
 # Create chemicals here
 ozo_chemicals = tmo.Chemicals(
@@ -47,72 +48,7 @@ Epoxide = create_new_chemical('Epoxy_stearic_acid',
     CAS = '2443-39-2'
  )
 for chemical in ozo_chemicals: chemical.default()
-
-tmo.settings.set_thermo(ozo_chemicals)
  
-
-
-
-
-#%% Stream Data and Mass balance
-mixed_feed_stream = tmo.Stream('mixed_feed_stream')
-mixed_feed_stream.imol['Oleic_acid']=0.86
-mixed_feed_stream.imol['H2O2']=6.85
-mixed_feed_stream.imol['H2O']=27.1
-# Ozonolysis_series_rxn(mixed_feed_stream)
-# print(mixed_feed_stream.F_mass)
-# mixed_feed_stream.show(N=100)
-
-# outs = [Stream('reactor_out')]
-#!!!TODO
-#Change conversion values
-#Check if you need diol data
-
-#%% Units
-
-import biosteam as bst
-
-class OzonolysisReactor(bst.BatchBioreactor):
-    _N_ins = 1
-    _N_outs = 1
-    @property
-    def effluent(self):
-        return self.outs[0]
-
-    
-    def __init__(self, ID='', ins=None, outs=(), thermo=None,
-                 tau=17, N=None, V=None, T=373.15, P=101325,
-                 Nmin=2, Nmax=36):
-        bst.BatchBioreactor.__init__(self, ID, ins, outs, thermo,
-                                   tau = tau , N = N, V = V, T = T, 
-                                   P =P ,Nmin =Nmin, Nmax = Nmax)
-    
-        
-        
-    def _setup(self):
-        self.reactions = Ozonolysis_parallel_rxn = tmo.SeriesReaction([
-             #Assumption, every conversion is 0.947 and overall conversion is (0.947^3)
-            tmo.Rxn('Oleic_acid + H2O2 -> Epoxy_stearic_acid+ Water ', 'Oleic_acid', X= 0.947),
-            tmo.Rxn('Epoxy_stearic_acid + H2O2 -> Nonanal + Oxononanoic_acid + H2O', 'Epoxy_stearic_acid', X = 0.947),
-            tmo.Rxn('Nonanal + Oxononanoic_acid + 2H2O2 -> Azelaic_acid + Nonanoic_acid+ 2H2O', 'Nonanal', X = 0.947)]
-        )
-        #Ozonolysis_parallel_rxn.correct_atomic_balance(['Oleic_acid','H2O2','9_10_epoxy_stearic_acid','H2O','Nonanal','9_Oxononanoic_acid','Nonaoic_acid','Azelaic_acid'])
-    
-    def _run(self):
-        feed = self.ins[0]
-        effluent = self.outs[0]
-        effluent.copy_like(feed)
-        self.reactions(effluent)
-        effluent.T = self.T
-        effluent.P = self.P
-        
-reactor = OzonolysisReactor(
-    ins = mixed_feed_stream, 
-    V=3785, # in m3 (equivalent to 1 MMGal)
-)
-reactor.simulate()
-print(reactor.results())
-reactor.show()
 
  
      
--- BioSTEAM 2.x.x/biorefineries/ozonolysis/units.py
+++ BioSTEAM 2.x.x/biorefineries/ozonolysis/units.py
@@ -6,6 +6,48 @@ Created on Fri Oct 29 08:17:38 2021
 """
 
 import biosteam as bst
+import thermosteam as tmo
 
-class OzonolysisReactor(bst.Unit):
-    pass
+class OzonolysisReactor(bst.BatchBioreactor):
+    _N_ins = 1
+    _N_outs = 1
+    @property
+    def effluent(self):
+        return self.outs[0]
+
+    
+    def __init__(self, ID='', ins=None, outs=(), thermo=None,
+                 tau=17, N=None, V=None, T=373.15, P=101325,
+                 Nmin=2, Nmax=36):
+        bst.BatchBioreactor.__init__(self, ID, ins, outs, thermo,
+                                   tau = tau , N = N, V = V, T = T, 
+                                   P =P ,Nmin =Nmin, Nmax = Nmax)
+        c = self.chemicals
+        self.reactant_conversion = 0.808
+        #self.nonanal_selectivity = 0.094
+        self.selectivity_epoxide = 0.071 * c.Oleic_acid.MW / c.Epoxy_stearic_acid.MW 
+        #self.oxononanoic_acid_selectivity = 0.029
+        #self.nonanoic_acid_selectivity = 0.277
+        self.selectivity_azelaic_acid = 0.442 * c.Oleic_acid.MW / c.Azelaic_acid.MW 
+        
+    def _setup(self):
+        # selectivity_azelaic_acid = selectivity_nonanoic_acid = X2 * X1
+        # selectivity_nonanal = selectivity_oxonanoic_acid = X1 * (1 - X2)
+        # selectivity_epoxide = (1 - X1)
+        X1 = 1 - self.selectivity_epoxide
+        X2 = self.selectivity_azelaic_acid / X1
+        self.reactions = tmo.SeriesReaction([
+            # Assumption, every conversion is 0.947 and overall conversion is (0.947^3)
+            tmo.Rxn('Oleic_acid + H2O2 -> Epoxy_stearic_acid+ Water ', 'Oleic_acid', X=self.reactant_conversion),
+            tmo.Rxn('Epoxy_stearic_acid + H2O2 -> Nonanal + Oxononanoic_acid + H2O', 'Epoxy_stearic_acid', X = X1),
+            tmo.Rxn('Nonanal + Oxononanoic_acid + 2H2O2 -> Azelaic_acid + Nonanoic_acid+ 2H2O', 'Nonanal', X = X2)]
+        )
+        
+    def _run(self):
+        feed = self.ins[0]
+        effluent = self.outs[0]
+        effluent.copy_like(feed)
+        self.reactions(effluent)
+        effluent.T = self.T
+        effluent.P = self.P
+        
