0b602c28a960af0bf630fdaefa19d5b95329fa90
lkudli2 <91288134+lkudli2@users.noreply.github.com>
2022-01-17 16:30:16 -0600

    added a distillation column

--- BioSTEAM 2.x.x/biorefineries/ozonolysis/chemicals_info.py
+++ BioSTEAM 2.x.x/biorefineries/ozonolysis/chemicals_info.py
@@ -8,48 +8,44 @@ from biosteam import Unit, Stream, settings, main_flowsheet
 
 # Chemicals - Definition
 
-#These chemicals are available in the database
+#For unavailable chemicals
+def create_new_chemical(ID, phase = 's', **constants):
+       na_chemical = tmo.Chemical(ID, search_db=False,
+                                  phase=phase, **constants)
+       ozo_chemicals.append(na_chemical)
+       return na_chemical
+
+#Chemicals that are available in the database
 ozo_chemicals = tmo.Chemicals(
     ['Water','Hydrogen_peroxide','Oleic_acid',
-     'Nonanal','Nonanoic_acid','Azelaic_acid'])
-
-#For unavailable solids
-def create_new_solid_chemical(ID, phase='s', **constants):
-    # Create a new solid chemical without any data
-    solid = tmo.Chemical(ID, search_db=False, phase=phase, **constants)
-
-    # Add chemical to the Chemicals object
-    ozo_chemicals.append(solid)
-
-    return solid
+     'Nonanal','Nonanoic_acid','Azelaic_acid',
+     'oxiraneoctanoic_acid,_3-octyl-'])
 
 #Solid Catalyst not available in the database
 #TODO.xxx Add ref for the below
-Catalyst = create_new_solid_chemical(
+Catalyst = create_new_chemical(
     'Phosphotungstic_acid',
-    formula="H3PW12O40", # Chemical Formula
+    formula="H3PW12O40",
     MW=2880.2,
     CAS='1343-93-7'
     )
-#For unavailable liquids
-def create_new_liquid_chemical(ID, phase='l', **constants):
-    # Create a new liquid chemical without any data
-    liquid = tmo.Chemical(ID, search_db=False, phase=phase, **constants)
-
-    # Add chemical to the Chemicals object
-    ozo_chemicals.append(liquid)
-
-    return liquid
 
 #Liquid chemical not available in the database
 #TODO.xxx Add ref for the below
-Oxononanoic_acid = create_new_liquid_chemical('Oxononanoic_acid',
+Oxononanoic_acid = create_new_chemical(
+   'Oxononanoic_acid',
     phase='l',
     Hf=-579480,
     formula = 'C9H16O3',
     MW = 172.22,
     CAS = '2553-17-5'
 )
+
+for chemical in ozo_chemicals: chemical.default()
+ozo_chemicals.compile()
+ozo_chemicals.set_synonym('oxiraneoctanoic_acid,_3-octyl-' ,'Epoxy_stearic_acid') 
+ozo_chemicals.show()
+
 # =============================================================================
 #Tried below alternatives for Oxononanoic acid
 #Recognised CAS but does not have Tb, P or Hf
@@ -57,59 +53,6 @@ Oxononanoic_acid = create_new_liquid_chemical('Oxononanoic_acid',
 #Methyl_oxo_nonanoate.show()
 # =============================================================================
 
-# =============================================================================
-# Adding data to the existing chemicals 
-def updating_existing_chemical(ID, phase='l', **constants):
-    # Update an existing chemical without any data
-    existing_chemical = tmo.Chemical(ID, search_db=True, phase=phase, **constants)
-    
-    # Add underdefined chemical to the Chemicals object
-    ozo_chemicals.append(existing_chemical)
-
-    return existing_chemical
-
-#TODO.xxx Find Hf for the below
-#Reference for boiling point:https://www.guidechem.com/encyclopedia/trans-9-10-epoxyoctadecanoic-a-dic125128.html
-#Reference for the compound: https://pubchem.ncbi.nlm.nih.gov/compound/15868
-Epoxy_stearic_acid = updating_existing_chemical(
-    '9,10-Epoxyoctadecanoic_acid',
-     phase='l',
-     #Hf,
-    formula = 'C18H34O3',
-    CAS = '2443-39-2',
-    Tb = 696.05,
-   )
-Epoxy_stearic_acid.show()
-
-# =============================================================================
-#Prospective alternative chemicals for epoxide 
-#Has tb, lacks Pt and Hf
-#cis_epoxy_stearic_acid = Chemical('24560-98-3')
-#cis_epoxy_stearic_acid.show()
-
-#Tried the below did't work
-#Oxiraneoctanoic acid, 3-octyl-, trans= '13980-07-9'
-#Does not P, Hf or Tb
-#Oxiran = Chemical('13980-07-9')
-#Oxiran.show()
-#a valid CAS number was recognized, but its not in the database
-#Methyl_epoxystearate =Chemical('2500-59-6')
-#Methyl_epoxystearate.show()
-#epoxy_stearic_acid_methyl_ester = Chemical('6084-76-0')
-#=============================================================================
-
-for chemical in ozo_chemicals: chemical.default()
-ozo_chemicals.compile()
-#TODO.XXX Change name for Epoxy_stearic_acid 
-ozo_chemicals.set_synonym('8-(3-octyloxiran-2-yl)octanoic acid','Epoxy_stearic_acid')
-ozo_chemicals.show()
-
-# =============================================================================
-#IS THIS NEEDED? 
-#(Water,Hydrogen_peroxide,Oleic_acid,
-# Nonanal,Nonanoic_acid,Azelaic_acid) = ozo_chemicals
-# =============================================================================
-     
 
 
 
--- BioSTEAM 2.x.x/biorefineries/ozonolysis/model.py
+++ BioSTEAM 2.x.x/biorefineries/ozonolysis/model.py
@@ -6,12 +6,14 @@ Created on Mon Dec 20 09:02:42 2021
 """
 from chaospy import distributions as shape
 import biosteam as bst
-from biorefineries.ozonolysis.systems import reactor, ozonolysis_sys
+from biorefineries.ozonolysis.systems import reactor, ozonolysis_sys,separator
 model = bst.Model(ozonolysis_sys)
 
-@model.metric(name = 'theoritical maximum feedstock price')
+@model.metric(name = 'theoritical_minimum_sellling_price')
 def theoritical_min_SP():
-    revenue = sum([i.cost for i in reactor.outs])
+    
+    revenue = sum([i.cost for i in separator.outs])
+    
     feedstock_volume = sum([i.get_total_flow(units = 'gal/hr') for i in reactor.ins])
     return revenue/feedstock_volume
 
@@ -19,8 +21,42 @@ def theoritical_min_SP():
                  distribution=shape.Uniform(0.8, 1))
 def set_conversion(X):
     reactor.reactant_conversion = X
+    
+    # reactor is the object, reactant conversion is not defined
+    
+#Nonanal    
+@model.parameter(name='Oleic Acid',
+                 distribution=shape.Uniform(6, 8))
+def set_product_price(X):
+    separator.outs[0].price = X
+    
+@model.parameter(name='Nonanal',
+                 distribution=shape.Uniform(20, 50))
+def set_product_price(X):
+    separator.outs[1].price = X
 
+@model.parameter(name='Nonanoic Acid',
+                 distribution=shape.Uniform(1, 10))
+def set_product_price(X):
+    separator.outs[2].price = X
+    
+@model.parameter(name='Azelaic Acid',
+                 distribution=shape.Uniform(20, 28))
+def set_product_price(X):
+    separator.outs[3].price = X
+    
+@model.parameter(name='Oxo_nonanoic_acid',
+                 distribution=shape.Uniform(1, 30))
+#Oxo_nonanoic is $290 for 50 mg,
+def set_product_price(X):
+    separator.outs[4].price = X  
 
+@model.parameter(name='oxiraneoctanoic_acid,_3-octyl-',
+                 distribution=shape.Uniform(6, 8))
+def set_product_price(X):
+    separator.outs[5].price = X
+  
+    
 import numpy as np
 np.random.seed(1234) # For consistent results
 N_samples = 50
@@ -32,9 +68,8 @@ model.table # All evaluations are stored as a pandas DataFrame
 print(model.table)
 
 
-#Code I tried on 12/29
 df_rho, df_p = model.spearman_r()
-df_rho['Biorefinery', 'theoritical maximum feedstock price']
-bst.plots.plot_spearman_1d(df_rho['Biorefinery', 'theoritical maximum feedstock price'],
+df_rho['Biorefinery', 'theoritical_minimum_sellling_price']
+bst.plots.plot_spearman_1d(df_rho['Biorefinery', 'theoritical_minimum_sellling_price'],
                            index=[i.describe() for i in model.parameters],
                            name='IMFP')
