6b69ab136645ab58a8b659d2b624fa0a463d2fe6
Yoel <yoelcortes@gmail.com>
2022-03-19 11:37:08 -0500

    update requirements

--- BioSTEAM 2.x.x/biorefineries/__init__.py
+++ BioSTEAM 2.x.x/biorefineries/__init__.py
@@ -7,16 +7,6 @@
 # for license details.
 """
 """
-import sys, biosteam
-
-version_components = sys.version.split('.')
-PY_MAJOR, PY_MINOR = int(version_components[0]), int(version_components[1])
-PY37 = (PY_MAJOR, PY_MINOR) >= (3, 7)
-version_components = biosteam.__version__.split('.')
-BST_MAJOR, BST_MINOR = int(version_components[0]), int(version_components[1])
-BST222 = (BST_MAJOR, BST_MINOR) >= (2, 22)
-del sys, version_components, PY_MAJOR, PY_MINOR, BST_MAJOR, BST_MINOR
-
-__version__ = '2.23.13'
+__version__ = '2.23.14'
 
 
