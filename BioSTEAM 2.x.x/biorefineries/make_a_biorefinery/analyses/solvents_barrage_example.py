210b75dbdd93af2664212fcba47a54fc20e3b629
sarangbhagwat <59667492+sarangbhagwat@users.noreply.github.com>
2022-06-05 17:27:05 -0500

    minor TAL and solvents_barrage updates

--- BioSTEAM 2.x.x/biorefineries/make_a_biorefinery/analyses/solvents_barrage_example.py
+++ BioSTEAM 2.x.x/biorefineries/make_a_biorefinery/analyses/solvents_barrage_example.py
@@ -11,19 +11,19 @@ import thermosteam as tmo
 
 #%% Initialize chemicals (no need to initialize solvent chemicals) and feed streams. 
 #   This can be done in an external module (e.g., your biorefinery) and you can simply import your feed stream.
-
-tmo.settings.set_thermo(['Water', '2,3-Butanediol', 'AceticAcid', 'Glycerol'])
+from biorefineries.TAL.chemicals_data import TAL_chemicals as chemicals
+tmo.settings.set_thermo(chemicals)
 stream = tmo.Stream('stream')
-stream.imass['Water'] = 1000.
-stream.imass['2,3-Butanediol'] = 50.
-stream.imass['AceticAcid'] = 20.
-stream.imass['Glycerol'] = 20.
+stream.imol['Water'] = 100.
+stream.imol['Octyl_3_5_dihydroxyhexanoate'] = 50.
+stream.imass['Octyl_5_hydroxyhexanoate'] = 50.
+stream.imass['HMTHP'] = 20.
 
 #%% Run
 
 run_solvents_barrage(stream=stream, # Stream from which you wish to extract the solute
-                     solute_ID='2,3-Butanediol', # solute chemical ID
-                     impurity_IDs=['AceticAcid', 'Glycerol'], # List of IDs of impurities in "stream" that you want get partitioning results for, other than water; note that all chemicals in the stream will affect LLE interaction effects, regardless of which chemicals are present in impurity_IDs
+                     solute_ID='Octyl_3_5_dihydroxyhexanoate', # solute chemical ID
+                     impurity_IDs=['HMTHP', 'HMTHP'], # List of IDs of impurities in "stream" that you want get partitioning results for, other than water; note that all chemicals in the stream will affect LLE interaction effects, regardless of which chemicals are present in impurity_IDs
                      T=50+273.15, # Temperature (K) at which you wish to run the solvents barrage; temperature (K) of "stream" by default
                      stream_modifiers='baseline_stream', # String: 'baseline_stream' to analyze the "stream" passed in arguments; 'impurity_free_stream' to remove the impurities listed in impurity_IDs before performing analyses; 'solute_in_pure_water' to analyze simply for the solute in pure water
                      solvent_IDs = [ # Use CAS numbers if common/iupac names are not recognized by the Chemicals package
@@ -44,16 +44,17 @@ run_solvents_barrage(stream=stream, # Stream from which you wish to extract the 
                                      'Nonanol',
                                      'Decanol',
                                      'Dodecanol',
-                                     '117-81-7', # CAS number for Dioctyl (Diethylhexyl) phthalate
+                                      '117-81-7', # CAS number for Dioctyl (Diethylhexyl) phthalate
                                      'Diethyl sebacate', # No Psat, Hvap
                                      # 'Glycerol',
                                      'Toluene',
                                      'Trioctylamine',
                                      'Isoamyl alcohol',
-                                     '5137-55-3', # CAS number for Aliquat 336
+                                     # '5137-55-3', # CAS number for Aliquat 336 (MethylTrioctylAmmoniumChloride)
                                      # 'Water',
                                      'Benzene',
                                      '143-28-2', # CAS number for Oleyl alcohol
                                      'Tetrahydrofuran'
-                                     ]
+                                     ],
+                     solvent_mol=500,
                      )
