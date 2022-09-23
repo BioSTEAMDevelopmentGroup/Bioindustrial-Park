95d77e6ceb2166d65e04678a435d0c95306a2cb9
lkudli2 <91288134+lkudli2@users.noreply.github.com>
2022-09-23 14:11:27 -0500

    latest_updates

--- biorefineries/oleochemicals/systems_baseline.py
+++ biorefineries/oleochemicals/systems_baseline.py
@@ -10,17 +10,61 @@ import flexsolve as flx
 import numpy as np
 import units_baseline
 import chemicals_baseline
-from biosteam import SystemFactory
-
+from biosteam import main_flowsheet as f
+from biosteam import units, SystemFactory
+from biorefineries.lipidcane import create_lipid_pretreatment_system 
+from biorefineries.lipidcane import create_transesterification_and_biodiesel_separation_system
+from biorefineries.lipidcane import units
+from biosteam.units.design_tools import compute_vacuum_system_power_and_cost
+from biosteam.units.design_tools import PressureVessel
 #This is based on the Novomont patent released in 2016
-# TODO.xxx needs a vent to remove water
 ##dihydroxylation_reaction to dihydroxylate the unsaturated feed
+
+##TODO.xxx check with Yoel if the Vmax is right (1MMGal in ft3), mentioned in literature are 80L and 50L
+# TODO.xxx  account for catalyst volume   
+@SystemFactory(
+    ID = 'crude_HOSO_oil_to_biodiesel',
+    ins=[dict(ID='crude_vegetable_oil',
+              Water=0.0184,
+              TAG=11.1,
+              PL=0.1),
+         dict(ID='acetone'),
+         dict(ID='pure_glycerine'),
+         ],
+    outs=[dict(ID='polar_lipids'),
+          dict(ID='wastewater_1'),
+          dict(ID='biodiesel'),
+          dict(ID='crude_glycerol'),
+          dict(ID='wastewater_2')],
+    fixed_outs_size = True,     
+              )
+
+def crude_HOSO_oil_to_biodiesel(ins,outs):
+    crude_vegetable_oil, acetone, pure_glycerine, = ins
+    polar_lipids,wastewater_1,biodiesel,crude_glycerol,wastewater_2, = outs
+    
+    lipid_pretreatment_system = create_lipid_pretreatment_system(
+            ins= [crude_vegetable_oil, 
+                   acetone,
+                   pure_glycerine],
+            outs= ['degummed',
+                   polar_lipids,
+                   wastewater_1],
+            mockup = False)
+
+    create_transesterification_and_biodiesel_separation_system(
+            ins= lipid_pretreatment_system.outs[0],
+            outs=[biodiesel, crude_glycerol,wastewater_2],
+            mockup = False)
+    
+ob0 = crude_HOSO_oil_to_biodiesel()
+ob0.simulate()
+ob0.show()
 
 @SystemFactory(
     ID = 'dihydroxylation_reaction',
     ins = [dict(ID='biodiesel',
-                Methyl_oleate = 100,
-                T = 298.15),
+                ),
            dict(ID='fresh_HP',
                 Hydrogen_peroxide = 100,
                 T = 298.15),
@@ -30,13 +74,14 @@ from biosteam import SystemFactory
            dict(ID = 'fresh_tungsetn_catalyst',
                 tungstic_acid = 100)],           
     outs = [dict(ID = 'diol_product'),
+            dict(ID = 'water_for_reuse')
             ],
     fixed_outs_size = True,     
               )
 
 def dihydroxylation_system(ins,outs):
     biodiesel, fresh_HP, water_for_dihydroxylation, fresh_tunsgten_catalyst = ins
-    diol_product, = outs
+    diol_product,water_for_reuse = outs
     
 #Feedtanks and pumps
 # Oleic_acid_feedtank
@@ -48,7 +93,6 @@ def dihydroxylation_system(ins,outs):
                       outs = 'biodiesel_to_reactor_mixer')
 
 # Fresh_Hydrogen_peroxide_feedtank
-##TODO.xxs add recycle if that works out
 
     T102 =  bst.units.StorageTank('T102',
                                ins = fresh_HP,
@@ -57,7 +101,6 @@ def dihydroxylation_system(ins,outs):
                       ins = T102-0,
                       outs = 'HP_to_mixer')
 # Fresh_water_feedtank
-#TODO.xxx add correct price for water
     T103  = bst.units.StorageTank('T103_1',
                               ins = water_for_dihydroxylation,
                               outs = 'fresh_water_to_pump')
@@ -96,28 +139,59 @@ def dihydroxylation_system(ins,outs):
                                T104-0),
                         outs = 'feed_to_heat_exchanger')
 
-               
-### TODO.xxx ask if this is a batch or a continuous reactor
-    R101_H = bst.units.HXutility('R101_H',
+    R101_H1 = bst.units.HXutility('R101_H1',
                              ins = M102-0,
                              outs = 'feed_to_oleochemicals_reactor',
-                             T = 62 + 273.15
+                             T = 100 + 273.15
                              )
     
     R101 = units_baseline.DihydroxylationReactor('R101',
-                                ins = R101_H-0, 
-                                outs = diol_product,
+                                ins = R101_H1-0, 
+                                outs = ('diol_product'),
                                 P = 101325,
-                                V=3785, 
+                                T = 62 + 273.15,
+                                V_max=133666, 
                                 # in m3 (equivalent to 1 MMGal), 
                                 # this is including catalyst volume
                                                               )
-# TODO.xxx  account for catalyst volume
-ob1 = dihydroxylation_system() 
+    R101_P1 = bst.units.Pump('R101_P1',
+                             ins = R101-0,
+                             outs = diol_product,
+                             P = 20*10)
+    #Vacuum system
+    vacuum_results =  compute_vacuum_system_power_and_cost(
+                      F_mass=0, F_vol=0,
+                      P_suction=0.10*10e5,
+                      vessel_volume= 133666,
+                      vacuum_system_preference='Steam-jet ejector'
+                      )
+    R101_H2 = bst.units.HXutility('R101_H2',
+                             ins = R101-1,
+                             outs = 'condensed_water',
+                             T = 20 + 273.15
+                             )
+    
+## TODO.xxx Check with Yoel if the following specs are right, ask him how to 
+## get all the water in the liquid phase
+## TODO.xxx Check with Yoel if the vent.P pressure set is right
+
+    C101 = bst.units.PolytropicCompressor('K', 
+                                          ins= R101_H2-0,
+                                          outs=water_for_reuse,
+                                          vle = True,
+                                          P=101325,
+                                          eta=1)  
+    
+
+ob1 = dihydroxylation_system(ins=ob0.outs[2]) 
 ob1.simulate()
 ob1.show()
     
-##TODO.xxx maybe change this to ContinuousReactor
+
+## oxidative_cleavage_system to cleave the dihydroxylated feed to 
+## produce the fatty acids (200 level)
+##TODO.xxx what kind of storage tank to use for cobalt acetate
+
 @SystemFactory(
     ID = 'oxidative_cleavage_reaction',
     ins = [dict(ID='vicinal_diol_product'),
@@ -125,7 +199,7 @@ ob1.show()
                 Cobalt_acetate = 0.015,
                 water = (1- 0.015),
                 units = 'kg/hr'),
-            dict(ID = 'pressurised_air',
+            dict(ID = 'air',
                 Oxygen = 21,
                 Nitrogen = 79,
                 units = 'kg/hr')],                                   
@@ -133,23 +207,24 @@ ob1.show()
             dict(ID = 'mixed_oxidation_products')],
     fixed_outs_size = True,     
               )
-## oxidative_cleavage_system to cleave the dihydroxylated feed to 
-## produce the fatty acids (200 level)
-def oxidative_cleavage_system(ins,outs):
-    vicinal_diol_product,cobalt_catalyst_stream,pressurised_air, = ins
-    vented_gas,mixed_oxidation_products, = outs
-### TODO.xxx feedtank needs to be added for cobalt acetate etc
 
+def oxidative_cleavage_system(ins,outs):
+    vicinal_diol_product,cobalt_catalyst_stream,air, = ins
+    vented_gas,mixed_oxidation_products, = outs
     
+    P201 = bst.units.Pump('P201',
+                          ins = air,
+                          outs = 'pressurised_air',
+                          P = 20*10e5)
     M201 = bst.units.Mixer('M201',
                         ins = (vicinal_diol_product,
                                 cobalt_catalyst_stream,
-                                pressurised_air),
+                                P201-0),
                         outs = 'feed_to_heat_exchanger')
     
     def adjust_flowrates():
         cobalt_catalyst_stream.F_mass = vicinal_diol_product.F_mass* 2/11.4
-        pressurised_air.F_mass = vicinal_diol_product.F_mass* 13/11.4
+        air.F_mass = vicinal_diol_product.F_mass* 13/11.4
     M201.add_specification(adjust_flowrates, run=True)       
  
     R201_H = bst.units.HXutility('R201_H',
@@ -157,24 +232,30 @@ def oxidative_cleavage_system(ins,outs):
                               outs = 'feed_to_oxidative_cleavage_reactor',
                               T = 60 + 273.15
                               )
-### TODO.xxx check with Yoel on whether the pressure of the reactor has to be set or the air stream    
+### TODO.xxx check whether the pressure of the reactor also has to be set
+### TODO.xx what to do with the vented gas, wrt the pressure
+### TODO.xx how to incorporate elements of a jet loop reactor
+
     R202 = units_baseline.OxidativeCleavageReactor('R202',
                                 ins = R201_H-0, 
                                 outs = (vented_gas,
-                                        mixed_oxidation_products),
-                                P = 5*10e5,
-                                V=3785, 
-                                # in m3 (equivalent to 1 MMGal), 
-                                # this is including catalyst volume
-                                                              )
-    
+                                        'mixed_oxidation_products'),
+                                P = 20*10e5,
+                                T = 60 + 273.15,
+                                V_max=133666
+                                )
+    R202_V1 = bst.units.IsenthalpicValve(ID = 'R202_V1',
+                                         ins = R202-1,
+                                         outs = mixed_oxidation_products,
+                                         P = 101325)
+        
 ob2 = oxidative_cleavage_system(ins = ob1.outs[0]) 
 ob2.simulate()
 ob2.show()
 
-### depressurise the vessel
-## organic_phase_separation to separate aqueous portion containing 
-## catalysts and fatty acids (300 level)
+
+# organic_phase_separation to separate aqueous portion containing 
+# catalysts and fatty acids (300 level)
 @SystemFactory(
     ID = 'organic_phase_separation',
     ins = [dict(ID='mixed_oxidation_products')],                  
@@ -186,13 +267,15 @@ ob2.show()
 def organic_phase_separation(ins,outs):
     mixed_oxidation_products, = ins
     organic_phase,aqueous_phase, = outs
+    
 ### TODO.xxx add the costs acc to disc separators
-### TODO.xxx figure out how to depressurise
     L301 = bst.units.LiquidsSplitCentrifuge('L301',
                                             ins= mixed_oxidation_products,
                                             outs=(organic_phase,
                                                   aqueous_phase),
-                                            split = ({'Hydrogen_peroxide': 0.0,                                               'Water': 0,
+                                            split = ({
+                                                'Hydrogen_peroxide': 0.0,   
+                                                'Water': 0.2,
                                                 'Pelargonic_acid' : 1,
                                                 'Methyl_oleate': 1,
                                                 'Monomethyl_azelate' : 1,
@@ -200,7 +283,7 @@ def organic_phase_separation(ins,outs):
                                                 'Nitrogen': 0.0,
                                                 'Oxygen': 0.0,
                                                 'tungstic_acid': 0,
-                                                'Cobalt_acetate': 0                               
+                                                'Cobalt_acetate': 0,
                                                 })
                                               )
     
@@ -208,8 +291,35 @@ ob3 = organic_phase_separation(ins = ob2.outs[1])
 ob3.simulate()
 ob3.show()
 
-##add degassing portion (400 level)
+### add degassing portion (400 level)
+### TODO.xxx check for other products that are produced propinoic, malonic, methanol etc
+### For now removing water is okay
+### Running flash drum under vacuum for degassing
+### Improve recovery of moisture
 
+@SystemFactory(
+    ID = 'degassing_the_oily_phase',
+    ins = [dict(ID='fatty_acids_with_some_moisture')],       
+    outs = [dict(ID = 'wastewater3'),
+            dict(ID = 'dried_crude_fatty_acids')
+            ],
+    fixed_outs_size = True,     
+              )
+def degassing_the_oily_phase(ins,outs):
+    fatty_acids_with_some_moisture, = ins
+    wastewater3,dried_crude_fatty_acids, = outs 
+    
+    F401 = bst.units.Flash(ins = fatty_acids_with_some_moisture,
+                           outs = (wastewater3,
+                                   dried_crude_fatty_acids),
+                           T = 75 + 273.15,
+                           P = 101325/2
+                                  )
+ob4 =  degassing_the_oily_phase(ins = ob3.outs[0])
+ob4.simulate()
+ob4.show()               
+ 
+    
 ##Nonanoic acid separation (500 level)
 @SystemFactory(
     ID = 'nonanoic_acid_separation',
@@ -256,48 +366,52 @@ def nonanoic_acid_fraction_separation(ins,outs):
                                           'Pelargonic_acid'),
                                   Lr = 0.95,
                                   Hr = 0.95,
+                                  P = 5000,
                                   k = 2,
                                   partial_condenser=False
                                   )
 
-ob5 = nonanoic_acid_fraction_separation(ins = ob3.outs[0]) 
+ob5 = nonanoic_acid_fraction_separation(ins = ob4.outs[0]) 
 ob5.simulate()
 ob5.show()
 
+## TODO.xxx how to incorporate three consecutive columns for hydrolysis
 ## Hydrolysis of FAME's to produce fatty acids (600 level)
+##TODO.xxx add other fatty acids as well
 @SystemFactory(
-    ID = 'hydrolysis_of_organic_fraction',
+    ID = 'azelaic_acid_production',
     ins = [dict(ID='crude_fatty_acids'),
             dict(ID ='water_for_emulsification',
-                Water = 100,
+                Water = 110,
                 units = 'kg/hr'),
             dict(ID ='resin_for_hydrolysis',
                 Amberlyst_catalyst = 100,
-                units = 100
+                units = 'kg/hr',
                 )],       
     outs = [dict(ID = 'azelaic_acid'),
-            dict(ID = 'oleic_acid')
+            dict(ID = 'vented_mixture')
             ],
     fixed_outs_size = True,     
               )
 def hydrolysis_of_organic_fraction(ins,outs):
     crude_fatty_acids,water_for_emulsification,resin_for_hydrolysis, = ins
-    azelaic_acid,oleic_acid, = outs
+    azelaic_acid,vented_mixture = outs
     
     M601 = bst.units.MixTank('M601',
                             ins = (crude_fatty_acids,
-                                  water_for_emulsification),
+                                  water_for_emulsification,
+                                  resin_for_hydrolysis),
                             outs = ('emulsified_mixture'))
                            
     def adjust_water_for_emuslification():
         water_for_emulsification = crude_fatty_acids.F_mass* 2
     M601.add_specification(adjust_water_for_emuslification, run=True)  
     
-    R601 = bst.units.ContinuousReactor(ins = (M601-0,
-                                              resin_for_hydrolysis
-                                              ))
+    R601 = units_baseline.Zeolite_packed_bed_reactor(ins = M601-0,
+                                                      outs = (azelaic_acid,
+                                                              vented_mixture),
+                                                      T = 60 + 273.15)
+    
 ob6 = hydrolysis_of_organic_fraction(ins = ob5.outs[0]) 
 ob6.simulate()
 ob6.show()
-
-
