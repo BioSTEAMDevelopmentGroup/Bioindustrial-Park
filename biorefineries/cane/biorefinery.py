fix units of area

biorefineries/cane/biorefinery.py
@@ -168,8 +168,8 @@ def YRCP2023():
 
 class Biorefinery:
     cache = {}
-    dry_biomass_yield = None # MT / hc
-    baseline_dry_biomass_yield = 25.62 # MT / hc 
+    dry_biomass_yield = None # MT / ha
+    baseline_dry_biomass_yield = 25.62 # MT / ha 
     set_feedstock_line = set_line_composition_parameters
     default_prices_correleted_to_crude_oil = False
     default_conversion_performance_distribution = 'longterm'
@@ -970,7 +970,7 @@ class Biorefinery:
         def biodiesel_production():
             return biodiesel_flow() / feedstock_consumption.get()
         
-        @metric(units='L/hc')
+        @metric(units='L/ha')
         def biodiesel_yield():
             if self.dry_biomass_yield is None: 
                 return None
@@ -1190,7 +1190,7 @@ class Biorefinery:
             self.update_dry_biomass_yield(biomass_yield)
             return 100. * tea.ROI - target
         
-        @metric(name='Competitive biomass yield', element='Feedstock', units='dry MT/hc')
+        @metric(name='Competitive biomass yield', element='Feedstock', units='dry MT/ha')
         def competitive_biomass_yield():
             if self.ROI_target is None: return np.nan
             if composition_specification.oil == 0: return self.baseline_dry_biomass_yield
@@ -1205,7 +1205,7 @@ class Biorefinery:
                     f, x0, x1, y0, y1, args=args
                 )
         
-        @metric(name='Energy competitive biomass yield', element='Feedstock', units='dry MT/hc')
+        @metric(name='Energy competitive biomass yield', element='Feedstock', units='dry MT/ha')
         def energy_competitive_biomass_yield():
             if self.net_energy_target is None: return np.nan
             NEP_target = self.net_energy_target

biorefineries/cane/contour_plots.py
@@ -312,7 +312,7 @@ def plot_metrics_across_composition_configurations(
     d1 = data[:, :, 1, :]
     metric_bars = [
         # MetricBar(MFPP.name, format_units(MFPP.units), colormaps[1], tickmarks(data[:, :, 0, :], 5, 1, expand=0, p=0.5), 18, 1),
-        MetricBar('Comp. biomass yield', 'Dry-'+format_units('MT/hc'), plt.cm.get_cmap('viridis_r'), tickmarks(d0[~np.isnan(d0)], 5, 1, expand=0, p=0.5), 10, 1),
+        MetricBar('Comp. biomass yield', 'Dry-'+format_units('MT/ha'), plt.cm.get_cmap('viridis_r'), tickmarks(d0[~np.isnan(d0)], 5, 1, expand=0, p=0.5), 10, 1),
         # MetricBar('Biod. prod.', format_units(BP.units), plt.cm.get_cmap('copper'), tickmarks(data[:, :, 1, :], 8, 1, expand=0, p=0.5), 10, 1),
         MetricBar('Net energy prod.', format_units(NEP.units), plt.cm.get_cmap('inferno'), tickmarks(d1[~np.isnan(d1)], 5, 1, expand=0, p=2), 20, 1),
     ]

biorefineries/cane/uncertainty_plots.py
@@ -159,7 +159,7 @@ mc_metric_settings = {
 
 mc_line_metric_settings = {
     'Biodiesel production': (biodiesel_production, f"Biodiesel production\n[{format_units('L/MT')}]"),
-    'Biodiesel yield': (biodiesel_yield, f"Biodiesel yield\n[{format_units('L/hc')}]"),
+    'Biodiesel yield': (biodiesel_yield, f"Biodiesel yield\n[{format_units('L/ha')}]"),
     'GWP biodiesel': (GWP_biodiesel, f"GWP Biodiesel [{GWP_units_L}]"),
     'ROI': (ROI, "Return on Investment [%]"),
 }
@@ -1129,7 +1129,7 @@ def _add_lines_biomass_yield_vs_oil_content():
     for name, color in zip(df.index, line_color_wheel):
         line = df.loc[name]
         oil = line['Stem oil (dw)']['Mean'] * 100
-        biomass = line['Biomass yield (dry MT/hc)']['Mean']
+        biomass = line['Biomass yield (dry MT/ha)']['Mean']
         # breakpoint()
         bst.plots.plot_scatter_points(
             [oil], [biomass], marker='X', s=100, color=color.RGBn,
@@ -1152,7 +1152,7 @@ def _plot_competitive_biomass_yield_across_oil_content(
     file = monte_carlo_file(configuration, across_lines=False, across_oil_content='oilcane vs sugarcane')
     df = pd.read_excel(file, sheet_name=features.competitive_biomass_yield.short_description, index_col=0)
     oil_content = np.array(df.columns) * 100
-    plt.ylabel(f"Biomass yield [dry-{format_units('MT/hc')}]")
+    plt.ylabel(f"Biomass yield [dry-{format_units('MT/ha')}]")
     if configuration == 'O2':
         oil_content = oil_content[1:]
         biomass_yield = df.iloc[:, 1:]