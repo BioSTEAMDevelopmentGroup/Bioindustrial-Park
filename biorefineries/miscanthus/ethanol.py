6ad773485aa21080b2c6a68c87b5c4b7f031ca59
Yalin Li <mailto.yalin.li@gmail.com>
2023-03-04 10:25:52 -0600

    add/update template scripts for miscanthus biorefineries

--- biorefineries/miscanthus/ethanol.py
+++ biorefineries/miscanthus/ethanol.py
@@ -10,11 +10,14 @@ from biorefineries.cornstover import ethanol_density_kggal
 from biorefineries.miscanthus import create_ethanol_system
 
 sys = create_ethanol_system()
+sys.simulate()
 tea = sys.TEA
 
 f = sys.flowsheet
 ethanol = f.stream.ethanol
 
 get_MESP = lambda: tea.solve_price(ethanol)*ethanol_density_kggal # from $/kg to $/gallon
-
 get_GWP = lambda: sys.get_net_impact('GWP')/sys.operating_hours/ethanol.F_mass*ethanol_density_kggal
+
+print(f'price: {get_MESP()}')
+print(f'GWP: {get_GWP()}')
