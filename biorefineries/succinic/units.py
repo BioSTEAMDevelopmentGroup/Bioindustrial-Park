11134e0ba65a4346b7f0709f81386db08a819e6b
sarangbhagwat <59667492+sarangbhagwat@users.noreply.github.com>
2023-05-07 21:27:20 -0500

    module header updates

--- biorefineries/succinic/units.py
+++ biorefineries/succinic/units.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug 23 12:11:15 2020
+# Bioindustrial-Park: BioSTEAM's Premier Biorefinery Models and Results
+# Copyright (C) 2021-, Sarang Bhagwat <sarangb2@illinois.edu>
+#
+# This module is under the UIUC open-source license. See
+# github.com/BioSTEAMDevelopmentGroup/biosteam/blob/master/LICENSE.txt
+# for license details.
 
 This module is a modified implementation of modules from the following:
 [i]	Bhagwat et al., Sustainable Production of Acrylic Acid via 3-Hydroxypropionic Acid from Lignocellulosic Biomass. ACS Sustainable Chem. Eng. 2021, 9 (49), 16659–16669. https://doi.org/10.1021/acssuschemeng.1c05441
