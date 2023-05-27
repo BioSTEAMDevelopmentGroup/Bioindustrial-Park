dd68aa6ee0ca3022cfcb6cab4ff593d8b1479c9a
Yoel <yoelcortes@gmail.com>
2023-05-27 15:32:21 -0500

    2.25.0 release

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
     name='biorefineries',
     packages=['biorefineries'],
     license='MIT',
-    version='2.24.0',
+    version='2.25.0',
     description="Biorefinery models in BioSTEAM",
     long_description=open('README.rst', encoding='utf-8').read(),
     author='Yoel Cortes-Pena',
-    install_requires=['biosteam>=2.35.0,<2.36.0'],
+    install_requires=['biosteam>=2.37.0,<2.38.0'],
     python_requires=">=3.9",
     package_data=
         {'biorefineries': ['biorefineries/*',
