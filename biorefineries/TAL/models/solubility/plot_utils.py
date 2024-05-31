6498268f08b66f9b5ba894513add46d75405adf9
sarangbhagwat <59667492+sarangbhagwat@users.noreply.github.com>
2024-05-31 16:23:55 -0500

    add minor ticks and increase font size for solubility plot

--- biorefineries/TAL/models/solubility/plot_utils.py
+++ biorefineries/TAL/models/solubility/plot_utils.py
@@ -13,6 +13,7 @@ Created on Fri Aug 18 17:32:13 2023
 """
 
 from matplotlib import pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 import numpy as np
 
 def plot_solubility_model(experimental_Ts, experimental_solubilities, 
@@ -25,17 +26,19 @@ def plot_solubility_model(experimental_Ts, experimental_solubilities,
                           filename='solubility_plot.png',
                           ylabel=r"$\bfTAL$" +" " +r"$\bfSolubility$" +" " + r"$\bfin$" +" " + r"$\bfWater$",
                           xlabel=r"$\bfTemperature$",
-                          solubility_units= r"$\mathrm{g-TAL}\cdot\mathrm{L-water}^{-1}$",
+                          solubility_units= r"$\mathrm{g-TAL}\cdot\mathrm{L}^{-1}$",
                           temperature_units="\u00b0" + r"$\mathrm{C}$",
                           units_on_newline=(False, True), # (x, y)
                           legend_loc='center left',
                           fontname="Arial Unicode",
-                          fontsize="12",
+                          fontsize="14",
                           predicted_plot_color='black',
                           experimental_plot_color='#63C6CE', 
                           experimental_plot_marker='x',
                           dpi=600,
                           bbox_inches='tight',
+                          xlabel_fontsize=16,
+                          ylabel_fontsize=16,
                           ):
 
 
@@ -43,6 +46,8 @@ def plot_solubility_model(experimental_Ts, experimental_solubilities,
     plt.rcParams['font.size'] = fontsize
     
     model_solubilities = [get_TAL_solubility_in_water_gpL(T) for T in model_Ts]
+    
+    fig, ax = plt.subplots()
     
     plt.plot(model_Ts-273.15, model_solubilities, color=predicted_plot_color, 
              label='predicted', zorder=1)
@@ -56,9 +61,58 @@ def plot_solubility_model(experimental_Ts, experimental_solubilities,
     for i in range(len(units_opening_brackets)):
         if units_on_newline[i]:
             units_opening_brackets[i] = "\n[" 
-            
-    plt.xlabel(xlabel +" " + units_opening_brackets[0] + temperature_units + "]")
-    plt.ylabel(ylabel +" " + units_opening_brackets[1] + solubility_units + "]")
+    
+    plt.xlabel(xlabel +" " + units_opening_brackets[0] + temperature_units + "]", fontsize=xlabel_fontsize)
+    plt.ylabel(ylabel +" " + units_opening_brackets[1] + solubility_units + "]",  fontsize=ylabel_fontsize)
+    
+    
+    plt.tick_params(
+        axis='y',          # changes apply to the y-axis
+        which='both',      # both major and minor ticks are affected
+        direction='out',
+        # right=True,
+        width=0.65,
+        labelsize=fontsize,
+        # zorder=200,
+        )
+    
+    plt.tick_params(
+        axis='y',          
+        which='major',      
+        length=7,
+        )
+
+    plt.tick_params(
+        axis='y',          
+        which='minor',      
+        length=3.5,
+        )
+    
+    plt.tick_params(
+        axis='x',          # changes apply to the y-axis
+        which='both',      # both major and minor ticks are affected
+        direction='out',
+        # right=True,
+        width=0.65,
+        labelsize=fontsize,
+        # zorder=200,
+        )
+
+    plt.tick_params(
+        axis='x',          
+        which='major',      
+        length=7,
+        )
+
+    plt.tick_params(
+        axis='x',          
+        which='minor',      
+        length=3.5,
+        )
+    
+
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    ax.yaxis.set_minor_locator(AutoMinorLocator())
     
     plt.xlim(xlim[0], xlim[1])
     plt.ylim(ylim[0], ylim[1])
