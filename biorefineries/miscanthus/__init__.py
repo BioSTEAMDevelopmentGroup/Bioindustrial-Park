d3e2babdcfd1554437e41df053a9d88fcca47431
emilypl2 <85245728+emilypl2@users.noreply.github.com>
2024-07-29 13:22:43 -0500

    Removing unnecessary files

--- biorefineries/miscanthus/__init__.py
+++ biorefineries/miscanthus/__init__.py
@@ -9,50 +9,11 @@
 
 from . import (
     systems,
-    _tea,
-    _process_settings,
-    _chemicals,
-    units
     )
+
+from .systems import *
 
 __all__ = (
     *systems.__all__,
-    *_tea.__all__,
-    *_process_settings.__all__,
-    *_chemicals.__all__,
-    'flowsheet',
-    'Area100',
-     'Area200',
-     'Area300',
-     'Area400',
-     'Area500',
-     'Area600',
-     'Area700',
-     'Area800',
-     'AllAreas',
-     'areas',
     )
 
-from .units import *
-from ._process_settings import *
-from ._chemicals import *
-from .systems import *
-from ._tea import *
-from biorefineries.cellulosic import Biorefinery
-_biorefinery_loaded = False
-
-
-def load():
-    global _biorefinery_loaded
-    br = Biorefinery('miscanthus ethanol')
-    dct = globals()
-    dct.update(br.__dict__)
-    dct['chemicals'] = br.chemicals
-    _biorefinery_loaded = True
-
-def __getattr__(name):
-    if not _biorefinery_loaded:
-        load()
-        dct = globals()
-        if name in dct: return dct[name]
-    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
