e1658c0495ad73e03446e14844ba543740a91016
wenjung2 <wenjung2@illinois.edu>
2025-01-06 16:55:54 -0600

    Update in MeOH synthesis

--- biorefineries/ccu/_units.py
+++ biorefineries/ccu/_units.py
@@ -15,11 +15,53 @@ from biosteam.units.decorators import cost
 from biosteam.units.design_tools import size_batch
 from thermosteam import MultiStream
 from biosteam.units.design_tools.geometry import cylinder_diameter_from_volume
+from qsdsan._sanunit import SanUnit
 
 Rxn = tmo.reaction.Reaction
 ParallelRxn = tmo.reaction.ParallelReaction
 
 #%% 
+# =============================================================================
+# H2 production through Liquid Alkaline (LA) electrolysis 
+# =============================================================================
+# Model H2 production by H2O => H2 + 0.5O2
+class Electrolyzer(SanUnit): 
+    _N_ins = 1
+    _N_outs = 2
+    
+    def __init__(self, ID="", ins=None, outs=(),
+                 stack_lifetime=10,
+                 renewable_electricity_price=0.03, # currently possible in U.S. markets with plentiful  wind
+                 **kwargs):
+        bst.Unit.__init__(self, ID, ins, outs)
+        self.stack_lifetime = stack_lifetime
+        self.renewable_electricity_price = renewable_electricity_price
+        
+    def _run(self):
+        water, = self.ins
+        hydrogen, oxygen = self.outs
+        hydrogen.phase = 'g'
+        hydrogen.P = 30 * 101325 # Model H2 purification so that output is 99.99% purity and 30 bar
+        oxygen.phase = 'g'
+        hydrogen.imol['H2'] = water.F_mol
+        oxygen.imol['O2'] = water.F_mol / 2
+    
+    def _cost(self): # based on https://www.osti.gov/biblio/2203367 P17
+        self.power_utility.rate = self.outs[0].imass['H2'] * 122000 * 24 / 50000 # base case 50 MTD / 122 MW
+        # self.power_utility.cost = self.renewable_electricity_price * self.power_utility.rate
+        self.baseline_purchase_costs['Stack'] = self.power_utility.rate * 292 # process equipment, piping, valves, and  instrumentation including temperature, pressure, flow, and level indicators
+        self.baseline_purchase_costs['Mechanical BOP'] = self.power_utility.rate * 280
+        self.baseline_purchase_costs['Electrical BOP'] = self.power_utility.rate * 155 # for AC-> DC, transformer, power substation
+        self.add_OPEX = self._calculate_replacement_cost()
+    
+    def _calculate_replacement_cost(self):
+        stack_replacement_cost  = self.baseline_purchase_costs['Stack'] / self.stack_lifetime / (365*24) # convert from USD/year to USD/hour
+        return stack_replacement_cost
+    
+
+
+    
+
 # =============================================================================
 # MeOH synthesis process
 # =============================================================================
@@ -33,21 +75,27 @@ class MeOH_SynthesisReactor(bst.units.design_tools.PressureVessel, bst.Unit):
               'Catalyst weight': 'kg'}
     
     _F_BM_default = {**bst.design_tools.PressureVessel._F_BM_default,
-                     'Catalyst': 1.0}
+                     'Catalyst': 1.0} # Cu/ZnO/Al2O3
     
-    def __init__(self, ID="", ins=None, outs=(), thermo=None, *,
-                 P=7600000,
+    def __init__(self, ID="", ins=None, outs=(),
+                 P=7600000/6894.76,
                  vessel_material='Stainless steel 304',
-                 vessel_type='Horizontal',
-                 length_to_diameter=3.0,
-                 **wargs):
-            bst.Unit.__init__(self, ID, ins, outs, thermo)
-            self.length_to_diameter = length_to_diameter #: Length to diameter ratio
+                 vessel_type='Vertical',
+                 length_to_diameter=0.15/0.016, # 10.1016/j.jclepro.2013.06.008
+                 WHSV = 0.028 * 3600 / 34.8, # Residence time in kg/hr feed / kg catalyst; 10.1016/j.jclepro.2013.06.008
+                 catalyst_density = 1775, # In kg/m^3; 10.1016/j.jclepro.2013.06.008
+                 catalyst_price = 13,
+                 porosity = 0.5,
+                 **kwargs):
+            bst.Unit.__init__(self, ID, ins, outs)
+            self.P = P
             self.vessel_material = vessel_material # Vessel material
             self.vessel_type = vessel_type # 'Horizontal' or 'Vertical'
-            self.catalyst_density = 1059.52 # In kg/m^3
+            self.length_to_diameter = length_to_diameter #: Length to diameter ratio
+            self.WHSV = WHSV
+            self.catalyst_density = catalyst_density
             self.catalyst_price = 13 # In USD/kg
-            self.P = P
+            self.porosity = 0.5 #  fraction of the bed volume occupied by void space
             self.reactions = ParallelRxn([
                 # Reaction definition                 Reactant    Conversion
                 Rxn('CO2 + 3H2 -> CH3OH + H2O',       'CO2',      0.210),
@@ -56,19 +104,20 @@ class MeOH_SynthesisReactor(bst.units.design_tools.PressureVessel, bst.Unit):
     
     def _run(self):
         feed, = self.ins
+        feed.phase = 'g'
         effluent, = self.outs
-        effluent.P = self.P
         effluent.copy_like(feed)
         self.reactions.adiabatic_reaction(effluent)
         
     def _design(self):
+        feed, = self.ins
         effluent, = self.outs
         length_to_diameter = self.length_to_diameter
         P = effluent.get_property('P', 'psi')
         results = self.design_results
-        catalyst = effluent.F_mass / self.WHSV
+        catalyst = feed.F_mass / self.WHSV
         results['Catalyst weight'] = catalyst
-        results['Volume'] = volume = catalyst / self.catalyst_density * 35.3147 # to ft3
+        results['Volume'] = volume = catalyst / self.catalyst_density / (1-self.porosity) * 35.3147 # to ft3
         D = cylinder_diameter_from_volume(volume, length_to_diameter)
         L = length_to_diameter * D
         results.update(self._vessel_design(P, D, L))
@@ -79,6 +128,5 @@ class MeOH_SynthesisReactor(bst.units.design_tools.PressureVessel, bst.Unit):
             self._vessel_purchase_cost(D['Weight'], D['Diameter'], D['Length'])
         )
         self.purchase_costs['Catalyst'] = self.catalyst_price * D['Catalyst weight']
-
 
 
--- biorefineries/ccu/system.py
+++ biorefineries/ccu/system.py
@@ -21,29 +21,81 @@ bst.main_flowsheet.set_flowsheet(F)
 
 tmo.settings.set_thermo(chems, cache=True)
 
+water_stream = Stream('water_stream',
+                      Water=1,
+                      phase='l',
+                      total_flow=10000,
+                      units='kg/hr')
+
 CO2_stream = Stream('CO2_stream',
                     CO2=1,
-                    total_flow=41000.000,
+                    phase='g',
+                    total_flow=41000,
                     units='m3/hr')
                       
-H2_stream = Stream('H2_stream',
-                   H2=1,
-                   phase='g',
-                   P=2500000,
-                   total_flow=123000,
-                   units='m3/hr')
+# H2_stream = Stream('H2_stream',
+#                    H2=1,
+#                    phase='g',
+#                    P=2500000,
+#                    total_flow=123000,
+#                    units='m3/hr')
 
-C1101 = units.MultistageCompressor('C1101', ins=CO2_stream, outs='', n_stages=4, pr=2.95, eta=0.75, vle=True)
+# CO2 compressing
+ks = [bst.units.IsentropicCompressor(P=3*101325), 
+      bst.units.IsentropicCompressor(P=8*101325),
+      bst.units.IsentropicCompressor(P=26.3*101325)]
 
-C1102 = units.IsothermalCompressor('C1102', ins=H2_stream, outs='', P=30*101325, vle=True)
+hxs = [bst.units.HXutility(T=T) for T in [38+273.15, 38+273.15, 38+273.15]]\
 
-M1101 = units.Mixer('M1101', ins=(C1101-0, C1102-0), outs='', rigorous=True)
+C1101 = units.MultistageCompressor('C1101', ins=CO2_stream, outs='', compressors=ks, hxs=hxs)
 
-H1101 = units.HXutility('H1101', ins=M1101-0, outs='', T=210+273.15, rigorous=True)
+C1102 = units.IsentropicCompressor('C1102', ins=C1101-0, outs='', P=78*101325)
 
-R1101 = _units.MeOH_SynthesisReactor('R1101', ins=(H1101-0), outs=('',))
+# Water electrolysis
+R1101 = _units.Electrolyzer('R1101', ins=water_stream, outs=('hydrogen','oxygen'))
+@R1101.add_specification(run=True)
+def adjust_water_flow():
+    C1101.run()
+    R1101.ins[0].F_mol = C1101.ins[0].F_mol * 3 # H2:CO2 = 3:1
 
-H1102 = units.HXutility('H1102', ins=R1101-0, outs='', T=35+273.15, vle=True)
+# H2 compressing to same pressure
+C1103 = units.IsentropicCompressor('C1103', ins=R1101-0, outs='', P=78*101325, vle=True)
 
-for i in R1101.reactions._reactant_index:
-    print(i, H1101.outs[0].mol[i])
+M1101 = units.Mixer('M1101', ins=(C1102-0, C1103-0), outs='', rigorous=True)
+
+M1102 = units.Mixer('M1102', ins=(M1101-0, ''), outs='')
+
+H1101 = units.HXprocess('H1101', ins=(M1102-0, ''), outs=('', ''))
+
+R1102 = _units.MeOH_SynthesisReactor('R1102', ins=H1101-0, outs='product')
+
+S1101 = units.Splitter('S1101', ins=R1102-0, outs=(1-H1101, ''), split=0.6)
+
+V1101 = units.IsenthalpicValve('V1101', ins=H1101-1, outs='', P=73.6*101325)
+
+M1103 = units.Mixer('M1103', ins=(V1101-0, ''), outs='')
+
+H1102 = units.HXutility('H1102', ins=M1103-0, outs='', T=35+273.15, rigorous=True)
+
+S1102 = units.PhaseSplitter('S1102', ins=H1102-0, outs=('gas', 'condensed_water_and_methanol'))
+
+S1103 = units.Splitter('S1103', ins=S1102-0, outs=('purge', 'recycled'), split=0.01)
+
+C1104 = units.IsentropicCompressor('C1104', ins=S1103-1, outs=1-M1102, P=78*101325)
+
+# For condensed water and methanol in S1102 (composed of methanol, water and residual dissolved gases)
+V1102 = units.IsenthalpicValve('V1102', ins=S1102-1, outs='', P=10*101325)
+
+V1103 = units.IsenthalpicValve('V1103', ins=V1102-0, outs='', P=1.2*101325)
+
+F1101 = units.Flash('F1101', ins=V1103-0, outs=('gas', ''), T=22+273.15, P=1.2*101325)
+
+H1103 = units.HXprocess('H1103', ins=(F1101-1, S1101-1), outs=('', 1-M1103))
+
+D1101 = units.BinaryDistillation('D1101', ins=H1103-0, outs=('gas_MEOH', 'bottom_water'),
+                                 LHK=('Methanol', 'Water'),
+                                 Lr=0.999, Hr=0.999, k=2,
+                                 is_divided=True)
+
+
+sys = bst.main_flowsheet.create_system('MeOH_sys')
