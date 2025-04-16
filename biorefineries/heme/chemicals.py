d7b7099489504869908e279dd16dd3d567cd3446
PO5W <peng.ouwen@u.nus.edu>
2025-04-16 12:06:08 +0800

    chemical modif

---
+++ biorefineries/heme/chemicals.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2025-04-16 11:40:08
+
+@author: Dr. Ouwen Peng
+@title: Postdoctoral Researcher
+@institute: Illinois ARCS
+@email: ouwen.peng@iarcs-create.edu.sg
+"""
+
+# %%
+
+import biosteam as bst
