d3f9ad4d8c05c3407a2c613036191ac53e587bfc
sarangbhagwat <sarangb2@illinois.edu>
2026-01-04 18:52:02 -0800

    major updates to ibo fed-batch strategy spec

--- biorefineries/isobutanol/system.py
+++ biorefineries/isobutanol/system.py
@@ -10,7 +10,7 @@ import thermosteam as tmo
 import numpy as np
 from biorefineries import corn
 from biorefineries.isobutanol import units
-from nskinetics.examples.saccharomyces_cerevisiae_fermentation_etoh_ibo import te_r, reset_kinetic_reaction_system
+from nskinetics.examples.s_cerevisiae_ferm_fb_inhib_mod_ibo import te_r, reset_kinetic_reaction_system
 
 __all__ = ('corn_EtOH_IBO_sys',)
 
@@ -23,6 +23,8 @@ solvent_chem = 'Isopentyl acetate' # 1
 # solvent_chem = '2-ethyl hexanol' # 3
 
 chems.append(tmo.Chemical(solvent_chem))
+chems.append(tmo.Chemical('AceticAcid'))
+chems.append(tmo.Chemical('Acetaldehyde'))
 tmo.settings.set_thermo(chems)
 
 #%%
@@ -40,8 +42,6 @@ corn_EtOH_sys.simulate()
 f = corn_EtOH_sys.flowsheet
 
 parameters = settings.process_parameters
-
-
 
 #%% Add splitter and feed & spike evaporators, mixers, and heat exchangers
 
@@ -83,7 +83,7 @@ def adjust_M301_water():
     M301_ins_1.imol['Water'] = M301.water_to_sugar_mol_ratio * M301.ins[0].imol[V405_new.sugar_IDs].sum()
     M301._run()
     
-H301 = bst.units.HXutility('H301', ins=M301-0, T=32+273.15, rigorous=True)
+H301 = bst.units.HXutility('H301', ins=M301-0, outs=('glucose_initial_feed',), T=32+273.15, rigorous=True)
 
 ## Spike evaporator, pumps, mixer, and hx
 F302 = bst.MultiEffectEvaporator('F302', ins=S301-1, outs=('F302_l', 'F302_g'),
@@ -95,7 +95,8 @@ F302_cost = F302._cost
 @F302.add_specification(run=False)
 def F302_spec():
     feed = F302.ins[0]
-    if feed.F_mol and feed.imass['Water']/feed.F_mass > 0.2:
+    if feed.F_mol:
+    # and feed.imass['Water']/feed.F_mass > 0.2:
         F302._run()
         F302._design = F302_design
         F302._cost = F302_cost
@@ -117,7 +118,7 @@ def adjust_M302_water():
     M302_ins_1.imol['Water'] = M302.water_to_sugar_mol_ratio * M302.ins[0].imol[V405_new.sugar_IDs].sum()
     M302._run()
     
-H302 = bst.units.HXutility('H302', ins=M302-0, T=32+273.15, rigorous=True)
+H302 = bst.units.HXutility('H302', ins=M302-0, outs=('glucose_spike_feed',), T=32+273.15, rigorous=True)
 
 #%%
 V405_old = f.V405
@@ -136,13 +137,19 @@ V405_new = nsk.units.NSKFermentation('V405',
                                  ins=(H301-0, f.P404-0, H302-0), 
                                  kinetic_reaction_system=te_r,
                                  n_simulation_steps=2000,
-                                 map_chemicals_nsk_to_bst = {'s_glu': 'Glucose',
-                                                             'x': 'Yeast',
-                                                             's_EtOH': 'Ethanol',
-                                                             's_IBO': 'Isobutanol'},
+                                 map_chemicals_nsk_to_bst = {'[s_glu]': 'Glucose',
+                                                             '[x]': 'Yeast',
+                                                             '[s_EtOH]': 'Ethanol',
+                                                             '[s_IBO]': 'Isobutanol',
+                                                             '[s_acetate]': 'AceticAcid',
+                                                             '[s_acetald]': 'Acetaldehyde',
+                                                             },
                                  f_reset_kinetic_reaction_system=reset_kinetic_reaction_system,
                                  tau=3*24,
+                                 tau_max=3*24,
                                  sugar_IDs=('Glucose',),
+                                 # tau_update_policy=None,
+                                 tau_update_policy=('max', '[s_EtOH]'),
                                  perform_hydrolysis=False)
 
 V405_new-0-1-f.V409
@@ -174,7 +181,6 @@ corn_EtOH_IBO_sys_no_IBO_recovery = bst.System.from_units('corn_EtOH_IBO_sys_no_
                                                      V405_new])
 corn_EtOH_IBO_sys_no_IBO_recovery.simulate()
 
-
 #%% Add isobutanol recovery system - stage 1/2
 stillage = f.stillage
 
@@ -185,12 +191,26 @@ makeup_isopentyl_acetate = tmo.Stream('makeup_isopentyl_acetate')
 
 M401 = bst.Mixer('M401', ins=('', makeup_isopentyl_acetate), outs=('isopentyl_acetate_solvent'))
 
-@M401.add_specification(run=True)
+M401_design = M401._design
+M401_cost = M401._cost
+
+bypass_IBO_separation_conditions = [lambda: True] # if any return True, don't try to recover Isobutanol
+
+@M401.add_specification(run=False)
 def M401_adjust_makeup_solvent():
-    req = S401.mol_solvent_per_mol_carrier*S401.ins[0].imol['Water']
-    recycle = M401.ins[1]
-    makeup = M401.ins[0]
-    makeup.imol[solvent_chem] = max(0, req-recycle.imol[solvent_chem])
+    if not np.any([i() for i in bypass_IBO_separation_conditions]):
+        M401._design = M401_design
+        M401._cost = M401_cost
+        req = S401.mol_solvent_per_mol_carrier*S401.ins[0].imol['Water']
+        recycle = M401.ins[1]
+        makeup = M401.ins[0]
+        makeup.imol[solvent_chem] = max(0, req-recycle.imol[solvent_chem])
+        M401._run()
+    else:
+        M401._design = lambda: 0
+        M401._cost = lambda: 0
+        M401.ins[1].empty()
+        M401._run()
 
 # solvent_extraction_thermo = tmo.Thermo(chemicals=[i for i in chems if i.ID in ('Water', 'Isobutanol', solvent_chem)])
 
@@ -200,23 +220,37 @@ S401 = bst.MultiStageMixerSettlers('S401',
                                     top_chemical=solvent_chem,
                                     )
 S401.mol_solvent_per_mol_carrier = 0.2
+
+S401_design = S401._design
+S401_cost = S401._cost
+
 @S401.add_specification(run=False)
 def S401_partial_chems():
-    M401.simulate()
-    feed = S401.ins[0]
-    raffinate = S401.outs[1]
-    chems_included_lle = ('Water', 'Isobutanol', solvent_chem)
-    chems_excluded_lle = {}
-    for i in feed.chemicals:
-        if i.ID not in chems_included_lle:
-            chems_excluded_lle[i.ID] = feed.imol[i.ID]
-            feed.imol[i.ID] = 0.0
-    
-    S401.simulate()
-    
-    for k, v in chems_excluded_lle.items():
-        for stream in (feed, raffinate):
-            stream.imol[k] = v
+    if not np.any([i() for i in bypass_IBO_separation_conditions]):
+        S401._design = S401_design
+        S401._cost = S401_cost
+        M401.simulate()
+        feed = S401.ins[0]
+        raffinate = S401.outs[1]
+        chems_included_lle = ('Water', 'Isobutanol', solvent_chem)
+        chems_excluded_lle = {}
+        for i in feed.chemicals:
+            if i.ID not in chems_included_lle:
+                chems_excluded_lle[i.ID] = feed.imol[i.ID]
+                feed.imol[i.ID] = 0.0
+        
+        S401._run()
+        
+        for k, v in chems_excluded_lle.items():
+            for stream in (feed, raffinate):
+                stream.imol[k] = v
+        
+    else:
+        S401._design = lambda: 0
+        S401._cost = lambda: 0
+        S401.ins[1].empty()
+        S401.outs[0].empty() # extract
+        S401.outs[1].copy_like(S401.ins[0]) # raffinate
     
 
 # M401.simulate()
@@ -231,6 +265,20 @@ D401 = bst.BinaryDistillation('D401', ins=M402-0, outs=('D401_t', 'D401_b'), LHK
                               partial_condenser=False)
 D401-1-0-M401 # recycle
 
+D401_design = D401._design
+D401_cost = D401._cost
+@D401.add_specification(run=False)
+def D401_bypass_spec():
+    if D401.ins[0].F_mol:
+        D401._design = D401_design
+        D401._cost = D401_cost
+        D401._run()
+    else:
+        D401._design = lambda: 0
+        D401._cost = lambda: 0
+        
+        S401.outs[0] = S401.ins[0].copy()
+        
 # M402.simulate()
 # D401.simulate()
 # D401.show(N=100)
@@ -270,7 +318,7 @@ V514.isobutanol_price = 1.43
 def V514_update_IBO_price():
     V514._run()
     ibo = V514.outs[0]
-    ibo.price = V514.isobutanol_price * ibo.imass['Isobutanol']/ibo.F_mass
+    if ibo.F_mol: ibo.price = V514.isobutanol_price * ibo.imass['Isobutanol']/ibo.F_mass
 
 #%% Remove all existing HXprocess units
 
@@ -329,7 +377,7 @@ fbs_spec = nsk.units.FedBatchStrategySpecification(
     target_conc_sugars=100,
     threshold_conc_sugars=10,
     conc_sugars_feed_spike=600,
-    tau=72,
+    tau_max=72,
     fermentation_reactor=V405_new,
     splitter=S301,
     feed_evaporator=F301,
@@ -341,14 +389,28 @@ fbs_spec = nsk.units.FedBatchStrategySpecification(
     sugar_IDs=['Glucose',]
     )
 
-#%% Simulate and solve TEA
+#%% Initialize simulation and TEA
+r = V405_new.kinetic_reaction_system._te
 corn_EtOH_IBO_sys.simulate()
 print(corn_EtOH_IBO_sys_tea.solve_price(f.ethanol))
 
 fbs_spec.load_specifications(target_conc_sugars=100,
 threshold_conc_sugars=10,
 conc_sugars_feed_spike=600,
-tau=72)
+tau_max=72)
 
-corn_EtOH_IBO_sys.simulate()
-print(corn_EtOH_IBO_sys_tea.solve_price(f.ethanol))
+
+#%% Simulate and solve TEA
+
+r.max_n_glu_spikes = 15
+for i in range(3):
+    fbs_spec.load_specifications(target_conc_sugars=100,
+    threshold_conc_sugars=90,
+    conc_sugars_feed_spike=500,
+    tau_max=72)
+    
+    corn_EtOH_IBO_sys.simulate()
+    
+    print(corn_EtOH_IBO_sys_tea.solve_price(f.ethanol))
+
+V405_new.kinetic_reaction_system._te.plot()
